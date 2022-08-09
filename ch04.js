d4374e5abcd46b272e5bcb0fa93f8f7180ed2bc0
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-08-09 18:18:29 +0500

    Update ch04.js

--- ch04.js
+++ ch04.js
@@ -86,3 +86,51 @@ db.listingsAndReviews.find({ "amenities": {
                                            "Laptop friendly workspace" ]
                                          }
                             }).pretty()
+
+Find all documents with exactly 20 amenities which include all the amenities listed in the query array, and display their price and address:
+
+
+COPY
+db.listingsAndReviews.find({ "amenities":
+        { "$size": 20, "$all": [ "Internet", "Wifi",  "Kitchen", "Heating",
+                                 "Family/kid friendly", "Washer", "Dryer",
+                                 "Essentials", "Shampoo", "Hangers",
+                                 "Hair dryer", "Iron",
+                                 "Laptop friendly workspace" ] } },
+                            {"price": 1, "address": 1}).pretty()
+Find all documents that have Wifi as one of the amenities only include price and address in the resulting cursor:
+
+
+COPY
+db.listingsAndReviews.find({ "amenities": "Wifi" },
+                           { "price": 1, "address": 1, "_id": 0 }).pretty()
+Find all documents that have Wifi as one of the amenities only include price and address in the resulting cursor, also exclude ``"maximum_nights"``. **This will be an error:*
+
+
+COPY
+db.listingsAndReviews.find({ "amenities": "Wifi" },
+                           { "price": 1, "address": 1,
+                             "_id": 0, "maximum_nights":0 }).pretty()
+Switch to this database:
+
+
+COPY
+use sample_training
+Get one document from the collection:
+
+
+COPY
+db.grades.findOne()
+Find all documents where the student in class 431 received a grade higher than 85 for any type of assignment:
+
+
+COPY
+db.grades.find({ "class_id": 431 },
+               { "scores": { "$elemMatch": { "score": { "$gt": 85 } } }
+             }).pretty()
+Find all documents where the student had an extra credit score:
+
+
+COPY
+db.grades.find({ "scores": { "$elemMatch": { "type": "extra credit" } }
+               }).pretty()
