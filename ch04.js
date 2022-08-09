c6d827cf416e9717d2ad78b45b44e395f3e88355
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-08-09 19:06:36 +0500

    Update ch04.js

--- ch04.js
+++ ch04.js
@@ -134,3 +134,29 @@ Find all documents where the student had an extra credit score:
 COPY
 db.grades.find({ "scores": { "$elemMatch": { "type": "extra credit" } }
                }).pretty()
+
+use sample_training
+
+db.trips.findOne({ "start station location.type": "Point" })
+
+db.companies.find({ "relationships.0.person.last_name": "Zuckerberg" },
+                  { "name": 1 }).pretty()
+
+db.companies.find({ "relationships.0.person.first_name": "Mark",
+                    "relationships.0.title": { "$regex": "CEO" } },
+                  { "name": 1 }).count()
+
+
+db.companies.find({ "relationships.0.person.first_name": "Mark",
+                    "relationships.0.title": {"$regex": "CEO" } },
+                  { "name": 1 }).pretty()
+
+db.companies.find({ "relationships":
+                      { "$elemMatch": { "is_past": true,
+                                        "person.first_name": "Mark" } } },
+                  { "name": 1 }).pretty()
+
+db.companies.find({ "relationships":
+                      { "$elemMatch": { "is_past": true,
+                                        "person.first_name": "Mark" } } },
+                  { "name": 1 }).count()
